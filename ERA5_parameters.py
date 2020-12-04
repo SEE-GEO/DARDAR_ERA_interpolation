08bec0f1b4709ac0aa7fb71cd0d00d1a7160cfb7
IKaur <ikaur1985@gmail.com>
2020-12-04 14:44:34 +0100

    added surface variables to the dict

--- ERA5_parameters.py
+++ ERA5_parameters.py
@@ -3,12 +3,29 @@
 """
 Created on Thu Dec  3 11:23:46 2020
 
+Dictionary containing long- and short-names of variables available 
+in ERA5 pressure or surface data.
+
+More information on the 
+single level variables can be found :
+https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview
+
+and 
+
+pressure levels can be found :
+    https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-pressure-levels?tab=overview
+    
+
 @author: inderpreet
 """
+
 
 
 parameters = {"temperature": "t",
                      "geopotential": "z",
                      "specific_humidity" : "q",
                      "specific_cloud_ice_water_content": "ciwc",
-                     "specific_cloud_liquid_water_content": "clwc"}
+                     "specific_cloud_liquid_water_content": "clwc",
+                     "10m_v_component_of_wind": "v10",
+                     "10m_u_component_of_wind": "u10",
+                     "skin_temperature": "skt",}
