ecd189f9176a30c209af21b4767a00c34b665323
IKaur <ikaur1985@gmail.com>
2020-12-18 16:02:50 +0100

    option to shift lon to correct range avoiding discontinuity

--- era2dardar/atmData.py
+++ era2dardar/atmData.py
@@ -5,32 +5,29 @@ Created on Wed Dec 16 09:18:07 2020
 
 class to convert ERA5 data to DARDAR grid
 
-the scripts downloads the matching ERA5 data in time 
-and 
-functions for different variables are provided.
-The returned outputs for each are in ARTS grid format and can be saved to xml
-using typhon xml functions
 
 @author: inderpreet
 
 """
-import os
+
 import numpy as np
-from datetime import datetime, timedelta
 from era2dardar.ERA5 import ERA5p, ERA5s
 from era2dardar.utils.alt2pressure import alt2pres
 import typhon.physics.thermodynamics as thermodynamics 
-import typhon.physics.atmosphere as atmosphere
-import typhon.arts.xml as xml
 from typhon.topography import SRTM30
+from typhon import constants
 from era2dardar.utils.scale_vmr import scale_vmr
 from scipy import interpolate
+from era2dardar.utils.convert_lon import shift_lon
+
 
 class atmdata():
     """
     Class atmdata  which interpolates ERA5 data to DARDAR/Cloudsat grids;
     inputs are dardar/cloudsat class object and the pressure grid over 
     which values are to be interpolated
+    The returned outputs for each are in ARTS grid format and 
+    can be saved to xml using typhon xml functions
     """
     
 
@@ -42,18 +39,27 @@ class atmdata():
         dardar : a DARDAR class instance
         p_grid : np.array, the pressure grid over which ERA5 
         is to be interpolated. Units are in [Pa]
-        If None, then the ERA5 grid is used        
+        If None, then the ERA5 grid is used [hard coded right now]       
 
         Returns
         -------
         None.
 
         """        
-        
-        # time stamp of DARDAR data
+
         self.dardar  = dardar
-        self.p_grid  = p_grid        
-        self.lat     = dardar.latitude
+        self.p_grid  = p_grid
+ 
+       
+        if p_grid is None:
+            
+            # use ERA5 pressure grid
+            self.p_grid = np.array([   1,    2,    3,    5,    7,   10,   20,   30,   50,   70,  100,
+        125,  150,  175,  200,  225,  250,  300,  350,  400,  450,  500,
+        550,  600,  650,  700,  750,  775,  800,  825,  850,  875,  900,
+        925,  950,  975, 1000, 1150]) * 100 # [Pa]
+            
+
 
     @property    
     def t_0(self):
@@ -77,6 +83,32 @@ class atmdata():
         """
         return self.dardar.t_1
     
+    @property
+    def lon(self):
+        """
+        longitudes
+
+        Returns
+        -------
+        np.array containing the longitudes defining the DARDAR scene
+        the longitudes are shifted to range [-180, 180] or [0, 360]
+        depending on the range which avoids the discontinuity
+        """
+        longitude = self.dardar.longitude
+        longitude = shift_lon(longitude)
+        
+        return longitude
+    
+    @property
+    def lat(self):
+        """
+        latitudes
+
+        Returns
+        -------
+        np.array containing the latitudes defining the DARDAR scene
+        """
+        return self.dardar.latitude  
         
     @property    
     def temperature(self):
@@ -130,7 +162,7 @@ class atmdata():
     
     @property
     def abs_species(self):  
-        """
+        """        
         The absorption species are simply stored as a list of strings
 
         Returns
@@ -166,6 +198,27 @@ class atmdata():
         
         return z_surface
 
+    @property    
+    def z_field(self):
+        """
+        interpolated ERA5 geopotential heights fields to DARDAR grid
+        and pressure grid defined in self.p_grid
+
+        Returns
+        -------
+        grid_z : np.array containing the interpolated values
+        dimensions [p, lat, lon]
+
+        """
+        g           = constants.g
+        p           = self.p_grid        
+        var         = "geopotential"
+        ERA_t       = ERA5p(self.t_0, self.t_1, var)
+        grid_t      = ERA_t.interpolate(self.dardar, p* 0.01)
+        grid_t      = np.expand_dims(grid_t, 2) / g #[convert geopot. to alt]
+        
+        return grid_t
+    
     @property
     def skin_temperature(self):
         """
@@ -381,10 +434,74 @@ class atmdata():
         grid_iwc        = f(p)
         grid_iwc        = grid_iwc.T
         grid_iwc        = np.expand_dims(grid_iwc, axis = (0, 3))
-        grid_iwc        = np.expand_dims(grid_iwc, 3)   
-        
+       
         return grid_iwc                    
         
+    @property
+    def Z(self):
+        """
+        The reflectivity data from DARDAR interpolated to pressure grid defined in
+        p_grid
+        -------
+        grid_N2 : np.array containing the interpolated values in
+        dimensions [1, p, lat, lon]
+        """ 
+        
+        p               = self.p_grid   
+        
+        Z               = self.dardar.z
+        height_d        = self.dardar.height
+        p_grid_d        = alt2pres(height_d)
+        f               = interpolate.interp1d(p_grid_d, Z)
+        grid_z        = f(p)
+        grid_z        = grid_z.T
+        grid_z        = np.expand_dims(grid_z, axis = (0, 3))
+       
+        return grid_z      
+    
+    def variable_plevel(self, variable):
+        """
+        interpolated ERA5 pressure variable to DARDAR grid
+        and pressure grid defined in self.p_grid
+        
+        Parameters
+        -----------
+        variable : string, name of variable
+
+        Returns
+        -------
+        grid_z : np.array containing the interpolated values
+        dimensions [p, lat, lon]
+
+        """
+        p           = self.p_grid        
+        var         = variable
+        ERA_t       = ERA5p(self.t_0, self.t_1, var)
+        grid_t      = ERA_t.interpolate(self.dardar, p* 0.01)
+        grid_t      = np.expand_dims(grid_t, 2)         
+        return grid_t
+    
+    def variable_surface(self, variable):
+        """
+        interpolated ERA5 pressure variable to DARDAR grid
+        
+        Parameters
+        -----------
+        variable : string, name of variable
+
+        Returns
+        -------
+        grid_z : np.array containing the interpolated values
+        dimensions [lat, lon]
+
+        """
+        var           = variable
+        ERA_t         = ERA5s(self.t_0, self.t_1, var)
+        grid_t        = ERA_t.interpolate(self.dardar)
+        grid_t        = np.expand_dims(grid_t, axis = 1)
+        
+        return grid_t
+
             
 
         
--- era2dardar/utils/convert_lon.py
+++ era2dardar/utils/convert_lon.py
@@ -7,9 +7,71 @@ Created on Wed Dec  2 12:50:51 2020
 """
 import numpy as np
 
+def shift_lon(lon):
+    """
+    shifts longitudes to the range -[-180, 180] or [0, 360]  to avoid the
+    discontinuity arising at 360/0 or 180/-180
+    
+
+    Parameters
+    ----------
+    lon : np.array containing longitude values
+
+    Raises
+    ------
+    Exception
+        if longitude values are smaller than -180 or greater than 360
+
+    Returns
+    -------
+    np.array containing longitude values in the range avoiding the discontinuity. 
+
+    """
+    
+    if np.any(lon < -180.0) or np.any(lon > 360.0):
+        raise Exception("Longitude values not in valid range")
+        
+        
+    
+    lon_360     = lon_180_to_360(lon)
+    diff_360    = lon_360.max() - lon_360.min()
+    
+    lon_180     = lon_360_to_180(lon)
+    diff_180    = lon_180.max() - lon_180.min()
+
+    if diff_360 > diff_180:
+        
+        return lon_180
+    else:
+        return lon_360
+    
+    
+
 def lon_180_to_360(lon):
+    """
+    converts longitudes from range [-180, 180] to [0, 360]
+
+    Parameters
+    ----------
+    lon : np.array  or scaler
+    Returns
+    -------
+    np.array or scaler in the [0, 360 ] range
+
+    """
     return lon % 360
         
 
 def lon_360_to_180(lon):
+    """
+    converts longitudes from range [[0, 360] to 
+
+    Parameters
+    ----------
+    lon : np.array  or scaler
+    Returns
+    -------
+    np.array or scaler in the [-180, 180]  range
+
+    """
     return(lon + 180) % 360 - 180
