d20da2f6997e2048be2c5c52f691bd9eb656e334
IKaur <ikaur1985@gmail.com>
2021-01-07 14:06:54 +0100

    interface to run multiple files

--- scripts/demo.py
+++ scripts/demo.py
@@ -3,29 +3,27 @@
 """
 Created on Mon Dec 21 15:00:30 2020
 
+interface to generate xml files for multiple DARDARfiles
+
 @author: inderpreet
 """
 import os
 import glob
 import numpy as np
-from datetime import datetime, timedelta
-from era2dardar.DARDAR import DARDARProduct
-from era2dardar.ERA5 import ERA5p, ERA5s
-from era2dardar.atmData import atmdata
-from era2dardar.utils.alt2pressure import alt2pres
-import typhon.physics.thermodynamics as thermodynamics 
-import typhon.physics.atmosphere as atmosphere
-import typhon.arts.xml as xml
-from typhon.topography import SRTM30
-from era2dardar.utils.scale_vmr import scale_vmr
+
 from era2dardar.dardar2atmdata import dardar2atmdata
+from era2dardar.DARDAR import DARDARProduct
+from era2dardar.utils.alt2pressure import alt2pres
+import typhon.arts.xml as xml
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import shutil
 
 
 # pressure grid
-p_grid = alt2pres(np.arange(-100, 25000, 250))
+p_grid = alt2pres(np.arange(-700, 20000, 250))
+p_grid = np.concatenate([p_grid, np.array([30, 20, 10, 7, 5, 3, 2, 1]) * 100])
 
 # latitudinal extent
 latlims    = [-30, 30]
@@ -49,23 +47,48 @@ outpath = os.path.expanduser("~/Dendrite/Projects/IWP/")
 
 # file = os.path.expanduser("~/Dendrite/SatData/DARDAR/2015/06/03/DARDAR-CLOUD_v2.1.1_2015154193320_48403.hdf")
 
-file = os.path.expanduser("~/Dendrite/SatData/DARDAR/2009/06/03/DARDAR-CLOUD_v2.1.1_2009154021055_16481.hdf")
+file = os.path.expanduser("~/Dendrite/SatData/DARDAR/2009/07/01/DARDAR-CLOUD_v2.1.1_2009182140722_16896.hdf")
 
 
 # start the loop
 
-for dardarfile in dardarfiles[10:20]:
-    for N in ["A","D"]:
+for dardarfile in dardarfiles[1:10]:
+    for N in ["A","D_N", "D_S"]:
  
         try:
             dardar = DARDARProduct(dardarfile, latlims = latlims, node = N)
             dardar.plot_scene()
         except:
-            print ("descending pass not available")
-            continue
+            raise Exception("descending pass not available")
+        
+        print ("t_0, t_1", dardar.t_0, dardar.t_1)
+        
+        lon          = dardar.longitude 
+        lat          = dardar.latitude
+        
+        lat1         = np.around(lat.min() - 2)
+        lat2         = np.around(lat.max() + 2)
+        
+        lon1         = np.around(lon.min() - 2)
+        lon2         = np.around(lon.max() + 2)
+
+        if lon1 < -180:
+            lon1 = -180.0
+        if lon2 > 180:
+            lon2 = 180.0    
+
+ # when encountering prime meridian, download global data
+ # keeps interpolation simple          
+        if lon1 == -180 or lon2 == 180:
+            lon1 = -180.
+            lon2 =  180.
             
-# get all atmfields
-        atm_fields  = dardar2atmdata(dardar, p_grid)
+ # domain for which ERA5 data is downloaded           
+        domain  = [lat1, lat2, lon1, lon2]
+        
+        
+# get all atmfields as a directory
+        atm_fields  = dardar2atmdata(dardar, p_grid, domain = domain)
         
         date = dardar.filename2date()
         outdir = year + "_" + date.strftime("%3j") + "_" + date.strftime("%2H") + "_" + N      
@@ -75,7 +98,7 @@ for dardarfile in dardarfiles[10:20]:
             os.makedirs(outdir)
         
   
-# save xml files          
+# save xml files  to a zipped folder        
         for key in atm_fields.keys():
             
              filename = os.path.join(outdir,   key + ".xml")
@@ -83,9 +106,11 @@ for dardarfile in dardarfiles[10:20]:
              xml.save(atm_fields[key], filename)
 
         output_filename = os.path.basename(outdir) 
-        shutil.make_archive(outdir, 'zip', outdir)      
+        shutil.make_archive(outdir, 'zip', outdir)   
+        shutil.rmtree(outdir)
+        
  
-# remove downloaded ERA files to avoid memory build up     
+# remove downloaded ERA files  
     erafiles = glob.glob("ERA5/*/*")
     for f in erafiles:    
         os.remove(f)        
@@ -113,4 +138,9 @@ for dardarfile in dardarfiles[10:20]:
 
 # #lsm = atm.sea_ice_cover
 # #plt.plot(lat_d, lsm)
+# z = atm_fields["z_field"]
+# fig, ax = plt.subplots(1,1, figsize = [8, 8])
+# ax.plot(np.log(p_grid * 0.01), z[:, 1000, 0]/1000)
+# ax.plot(np.log(p_grid * 0.01), z[:, 2000, 0]/1000)
 
+# ax.plot(np.log(p_grid * 0.01), z[:, 3, 0]/1000)
