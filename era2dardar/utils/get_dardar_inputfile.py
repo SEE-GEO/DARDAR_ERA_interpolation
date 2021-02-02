fd5d11e4a3c406d00b81957c46458e212b63d0d9
IKaur <ikaur1985@gmail.com>
2021-02-02 15:28:35 +0100

    get name of dardar granule from zipfile

---
+++ era2dardar/utils/get_dardar_inputfile.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 13:59:31 2021
+
+@author: inderpreet
+"""
+
+import numpy as np
+from datetime import datetime
+import os
+import glob
+
+
+def filename2date(filename):
+        filename = os.path.basename(filename)
+        filename = filename.split("_")[2]
+        pattern = "%Y%j%H%M%S"
+        return datetime.strptime(filename, pattern)
+    
+def find_dardar(date):
+    """
+    get dardar filename from the zipfile name
+
+    Parameters
+    ----------
+    date : datetime object containing timestamp of DARDAR file
+    Returns
+    -------
+    dardarfile : string containing DARDAR filename
+
+    """
+    
+    inpath = os.path.expanduser("~/Dendrite/SatData/DARDAR")
+    
+    year  = date.strftime("%Y")
+    month = date.strftime("%m")    
+    jday  = date.strftime("%j")
+    day   = date.strftime("%d")
+    hour  = date.strftime("%H")
+    
+    dardarfile = (glob.glob(os.path.join(inpath, year, month, day , 
+                                         "*" + year + jday + hour + "*"  )))
+    
+    return dardarfile[0]
+    
+def zip2dardar(zfile):
+    
+    basefile   = os.path.basename(zfile)
+    filename   = basefile[:11]
+    pattern    = "%Y_%j_%H"
+    
+    date       = datetime.strptime(filename, pattern)
+
+    if "A" in basefile:
+        N = "A"
+    if "D_S" in basefile:
+        N = "D_S"
+    if "D_N" in basefile:
+        N = "D_N"        
+    
+    dardarfile = find_dardar(date)
+    
+    return dardarfile,  N
